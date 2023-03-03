44f78084881258cbcb230ee4290189a454ec39ce
nolan <nolan.gero@gmail.com>
2023-03-03 17:24:36 +0100

    class commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        // Example usage
+        double[][] inputs = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+        int[] targets = {-1, -1, -1, 1};
+
+
+
+        Perceptron perceptron = new Perceptron(2, 0.1);
+        System.out.println("Before training: ");
+
+        System.out.println(perceptron.predict(new double[]{0, 0}, true)); // -1
+        System.out.println(perceptron.predict(new double[]{0, 1}, true)); // -1
+        System.out.println(perceptron.predict(new double[]{1, 0},true)); // -1
+        System.out.println(perceptron.predict(new double[]{1, 1}, true)); // 1
+
+        perceptron.train(inputs, targets, 1000);
+
+        System.out.println("After training: ");
+
+        // Test predictions
+        System.out.println(perceptron.predict(new double[]{0, 0}, true)); // -1
+        System.out.println(perceptron.predict(new double[]{0, 1}, true)); // -1
+        System.out.println(perceptron.predict(new double[]{1, 0}, true)); // -1
+        System.out.println(perceptron.predict(new double[]{1, 1},true)); // 1
+    }
 }
--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Perceptron {
+public class Perceptron {
+    private double[] weights;
+    private double bias;
+    private double learningRate;
+
+    public Perceptron(int numInputs, double learningRate) {
+        this.weights = new double[numInputs];
+        this.bias = 0;
+        this.learningRate = learningRate;
+        for (int i = 0; i < numInputs; i++) {
+            this.weights[i] = Math.random(); // initialize weights randomly
+        }
+    }
+
+    public int predict(double[] inputs, boolean show) {
+        double activation = 0;
+        for (int i = 0; i < inputs.length; i++) {
+            activation += inputs[i] * weights[i];
+        }
+        activation += bias;
+        int result = activation >= 0 ? 1 : -1;
+        if(show){
+            System.out.println("Inputs: [" + inputs[0] + ", "+ inputs[1] + "]");
+            System.out.println("Result: " + result);
+        }
+        return result;
+    }
+
+    public void train(double[][] inputs, int[] targets, int numEpochs) {
+        for (int epoch = 0; epoch < numEpochs; epoch++) {
+            for (int i = 0; i < inputs.length; i++) {
+                int output = predict(inputs[i], false);
+                int error = targets[i] - output;
+                bias += learningRate * error;
+                for (int j = 0; j < weights.length; j++) {
+                    weights[j] += learningRate * error * inputs[i][j];
+                }
+            }
+        }
+    }
 }
